676574c41bb4b0920d67e022a6a6177f76363cf3
buddyboisen <74414849+buddyboisen@users.noreply.github.com>
2020-11-13 14:52:15 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function youtube(){
+  window.open("https://www.youtube.com");
+}
+
+function  roblox() {
+    window.open("https://www.roblox.com")
+}
+
+function google() {
+  window.open("https://www.google.com")
+}
+
+function clickspeed() {
+  window.open("https://clickspeedtest.com")
+}
